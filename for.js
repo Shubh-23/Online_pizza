8ed59c13128ddb32bba9c008434f486083f135bf
Shubham patidar <Shubhampatidar@inwizards.com>
2022-06-11 18:34:35 +0530

    demo project

---
+++ for.js
@@ -0,0 +1,42 @@
+item = {
+    '1': {
+      item: {
+        id: 1,
+        name: 'Margherita',
+        image: 'pizza.png',
+        size: 'small',
+        price: '250'
+      },
+      qty: 2
+    },
+    '2': {
+      item: {
+        id: 2,
+        name: 'Marinara',
+        image: 'pizza.png',
+        size: 'medium',
+        price: '300'
+      },
+      qty: 1
+    },
+    '3': {
+      item: {
+        id: 3,
+        name: 'Carbonara',
+        image: 'pizza.png',
+        size: 'small',
+        price: '200'
+      },
+      qty: 1
+    }
+  }
+arr = []
+
+  // item.forEach(element => {
+  //   console.log(element)
+    
+  // });
+
+
+  
+console.log(arr)
